f9fcfcbaaa9b3f938fb16a51a82c6b4cfb3ee211
ubuntu <ubuntu@ubuntu-vm.myguest.virtualbox.org>
2023-02-20 13:17:14 -0800

    Added admission Control, Modified the syscalls as required

--- proj1/kernel/rtmon.c
+++ proj1/kernel/rtmon.c
@@ -2,90 +2,121 @@
 #include <linux/syscalls.h>
 #include <linux/sched.h>
 #include <linux/hrtimer.h>
+#include <linux/math.h>
 
 static ktime_t ET;
 static ktime_t Period;
 
+// #ifndef NR_PID_MAX
+// #define NR_PID_MAX 32768
+// #endif
+
 
 SYSCALL_DEFINE3(set_rtmon, pid_t, pid, unsigned int, C_ms, unsigned int, T_ms)
 {
-    
-    // Check if the provided pid is valid and belongs to a running task PIDTYPE_PID is a constant that is used to specify the type of a process ID in the Linux kernel. Specifically, it is used to indicate that a given process ID is associated with a process and not a thread.
-    struct task_struct *task = get_pid_task(find_get_pid(pid), PIDTYPE_PID);
-    if (task == NULL){
-        printk("Task Doesn't Exist\n");
-        return -1;
-    }
-
-    // Check if the provided C_ms and T_ms values are within the allowed range
-    if (C_ms > 10000 || T_ms > 10000 || C_ms < 1 || T_ms < 1){
-        printk("Given Values are wrong\n");
-        return -1;
-    }
-
-    // Check if the task already has non-zero timing parameters set
-    if (task->C > 0 || task->T > 0){
-        printk("Task Exists and has non zero parameters\n");
-        return -1;
-    }
-    // Convert the provided C_ms and T_ms values to ktime_t values in nanoseconds
     ET = ktime_set(0, C_ms * NSEC_PER_MSEC);
-    Period = ktime_set(0, T_ms * NSEC_PER_MSEC);   
+    Period = ktime_set(0, T_ms * NSEC_PER_MSEC);
+    int R1,R2 = 0;
+    struct task_struct *p,*t;
+    //check the parameter contraints 
+    if(C_ms <=1000 && T_ms <= 1000 && C_ms >= 1 && T_ms >= 1){
+        for_each_process_thread(p,t){
+            if(t->pid == pid){
+                if(t->C != 0 && t->T !=0){
+                    printk("The task Timing parameters are non-zero\n");
+                    return -1;
 
-    task->C = ET;
-    task->T = Period; 
+                }else{
+                    R1 = ET;
+                    while (R1 != R2 && R2 <= Period){
+                        R1 = R2;
+                        R2 = ET;
+                        for_each_process_thread(p,t){
+                            if (t->C > 0 && t->T > 0 && pid_alive(t)){
+                                if (t->T < Period){
+                                    R2 += ((((R1) + (t->T) -1)/(t->T)) * t->C);
 
-    printk("The value of Period is : %lld\n", task->T);
-    printk("The Value of Execution Time is : %lld\n", task->C);
+                                }
 
-    return 0;
+                            }
+
+                        }
+                        
+                    }if(R1 == R2){
+                        for_each_process_thread(p,t){
+                            if(t->pid == pid){
+                                t->C = ET;
+                                t->T = Period;
+                                printk("The Values are added to the tcb %lld, %lld , %d\n", t->C, t->T, t->pid);
+                                return 0;
+                            }
+                        }
+                    }
+
+
+                }
+
+            }
+
+        }
+        printk("The Pid Does not Exist\n");
+        return -1;
+        
+    }else {
+        printk("The Values provided are invalid\n");
+        return -1;
+    }
 }
 
 SYSCALL_DEFINE1(cancel_rtmon, pid_t, pid)
 {
-    //Check if the provided pid is valid and belongs to a running task
-    struct task_struct *task = get_pid_task(find_get_pid(pid), PIDTYPE_PID);
-    if (task == NULL){
-        printk("Task Doesn't Exist\n");
-        return -1;
-    }
-    printk("%lld and %lld\n", task->C, task->T);
+    struct task_struct *p,*t;
+    for_each_process_thread(p,t){
+        if(t->pid == pid){
+            if(t->C !=0 && t->T !=0){
+                t->C = 0;
+                t->T = 0;
+                printk("Pids are set to zero\n"); 
+                return 0;               
+            }else{
+                printk("The timing parameters are already zero\n");
+                return -1;
+            }
 
-    //Check if the task already has zero timing parameters set
-    if (task->C == 0 && task->T == 0){
-        printk("Task is already cleared\n");
-        return -1;
-    }
-    //Clear the timing parameters in the task's TCB
-    task->C = 0;
-    task->T = 0;
+        }
 
-    return 0;
+        }
+        printk("The PID doesnot Exist\n");
+        return -1;
 }
+
 SYSCALL_DEFINE1(print_rtmon, pid_t, pid)
 {
+    struct task_struct *p,*t;
     if(pid >= 0){
-        struct task_struct *task = get_pid_task(find_get_pid(pid), PIDTYPE_PID);
-        if (task == NULL){
-            printk("Task Doesn't Exist\n");
-            return -1;
+        for_each_process_thread(p,t){
+            if(t->pid == pid){
+
+                ktime_t C_ms = (t->C/1000000);
+                ktime_t T_ms = (t->T/1000000);
+                printk("print_rtmon: PID %d, C %lld ms, T %lld ms\n", t->pid, C_ms, T_ms);
+                return 0;
+
+            }
+            }
+            printk("Task Doesnot Exist\n");
         }
-        ktime_t C_ms = (task->C/1000000);
-        ktime_t T_ms = (task->T/1000000);
-        printk("print_rtmon: PID %d, C %lld ms, T %lld ms\n", task->pid, C_ms, T_ms);
-        return 0;
-    }
 
     if(pid == -1){
-        struct task_struct *task;
-        for_each_process(task){
-            ktime_t C_ms = (task->C/1000000);
-            ktime_t T_ms = (task->T/1000000);
-            printk("print_rtmon: PID %d, C %lld ms, T %lld ms\n", task->pid, C_ms, T_ms);
+        for_each_process_thread(p,t){
+            ktime_t C_ms = (t->C/1000000);
+            ktime_t T_ms = (t->T/1000000);
+            printk("print_rtmon: PID %d, C %lld ms, T %lld ms\n", t->pid, C_ms, T_ms);
         }
     }else{
         printk("Error!!!!!");
         return -1;
     }
+
     return 0;
 }
